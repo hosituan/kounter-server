cf66f03174b2a32366f1a4c0e695177952953141
hosituanit <67009473+hosituanit@users.noreply.github.com>
2021-03-22 00:17:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from kountServer.server import app
+import logging
 if __name__ == "__main__":
   app.run()
+  app.logger.addHandler(logging.StreamHandler(sys.stdout))
+  app.logger.setLevel(logging.ERROR)
