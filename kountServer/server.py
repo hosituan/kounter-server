2abb8c22f467aeda2ef3583a1645ea8b4b7d0507
hosituanit <67009473+hosituanit@users.noreply.github.com>
2021-03-22 07:16:16 +0000

    Update server.py

--- kountServer/server.py
+++ kountServer/server.py
@@ -66,7 +66,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             #colorize
-            command = "python3 egg_kounter.py " + UPLOAD_FOLDER + "/" + filename
+            command = "python3 kountServer/egg_kounter.py " + UPLOAD_FOLDER + "/" + filename
             print(command)
             print(os.path.abspath(os.getcwd()))
             os.system(command)
