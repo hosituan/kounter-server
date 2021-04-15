e9486611011ba44bbda4ee981bf951b71e058475
hosituanit <hosituan.work@gmail.com>
2021-04-15 15:14:09 +0700

    fix router bug

--- kountServer/app.py
+++ kountServer/app.py
@@ -190,7 +190,7 @@ def countStep():
         if request.form.get("name") == "Chicken Egg":
           print("Start counting")
           emit("countResult", {
-            'success': True,
+            'success': "True",
             'message': 'Start counting'
           })
           showConfidence = False
@@ -242,53 +242,6 @@ def countStep():
       success=False,
       message="Only accept PNG, JPG, JPEG extension"
     )
-    if request.form.get("name") == "Chicken Egg":
-      filename = str(request.form.get("imageName"))
-      print("Start counting")
-      showConfidence = False
-      if int(request.form.get("showConfidence")) == 0:
-        showConfidence = True
-      startCountEggs(os.path.join(UPLOAD_FOLDER, filename), filename, showConfidence)
-      result_file = str(filename + "_result.jpg")            
-      read_dictionary = np.load(os.path.join(OUTPUT_FOLDER, filename+'_result.npy'),allow_pickle='TRUE').item()
-      count_value = read_dictionary[filename]
-      print("uploading")
-      url = upload_diary(result_file)
-      return jsonify(
-        success=True,
-        message="Counted",
-        name = "Chicken Egg",
-        fileName=filename,
-        url=url,
-        count=count_value
-      )
-    elif request.form.get("name") == "Fire Wood":
-      filename = str(request.form.get("imageName"))
-      print("Start counting")
-      showConfidence = False
-      if int(request.form.get("showConfidence")) == 0:
-        showConfidence = True
-      startCountWood(os.path.join(UPLOAD_FOLDER, filename), filename, showConfidence)
-      result_file = str(filename + "_result.jpg")            
-      read_dictionary = np.load(os.path.join(OUTPUT_FOLDER, filename+'_result.npy'),allow_pickle='TRUE').item()
-      count_value = read_dictionary[filename]
-      url = upload_diary(result_file)
-      return jsonify(
-        success=True,
-        message="Counted",
-        name = "Fire Wood",
-        fileName=filename,
-        url=url,
-        count=count_value
-      )
-    return jsonify(
-                success=False,
-                message="We can't count this type!"
-              )
-  return jsonify(
-                success=False,
-                message="This is GET method"
-              )
 
 
 def get_session():
